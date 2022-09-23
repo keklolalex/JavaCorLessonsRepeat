47f666c0740ed3a1542d6bebc6532cf988a2d786
alexmint <keklolpuck@gmail.com>
2022-09-23 19:39:18 +0300

    обновил репозиторий

---
+++ src/lesson2/Lesson2.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        //операторы сравнения
+        int a = 54;
+        int d = 454;
+        int c = 32;
+        System.out.println(a > d); //false
+        System.out.println(a >= c); //true
+        System.out.println(a = c); // 54
+        System.out.println(a != d); //true
+    }
+
+}
